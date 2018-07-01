5769c0e8c027edb9091df1d4bf37e8a36d9327fb
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-01 21:21:56 +0530

    Initial commit --Finished project

---
+++ animations/FoodController.swift
@@ -0,0 +1,30 @@
+//
+//  FoodController.swift
+//  animations
+//
+//  Created by Raghav Vashisht on 01/07/18.
+//  Copyright © 2018 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class FoodController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
